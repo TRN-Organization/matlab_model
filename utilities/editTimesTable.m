ba18c04b7bb1be5fb6ebfdd502e3aa40f63292d3
尾玉洵規 <xodama.junki@contract.isid.co.jp>
2023-11-07 11:28:44 +0900

    develop用意

--- utilities/editTimesTable.m
+++
@@ -1,6 +0,0 @@
-% Utility to open the timestable app for editing.
-
-%   Copyright 2018 MathWorks, Inc.
-
-edit('timestable.mlapp')
-
--- utilities/runTheseTests.m
+++
@@ -1,9 +0,0 @@
-function runTheseTests()
-% RUNTHESETESTS Run all tests in the project.
-
-%   Copyright 2018-2020 The MathWorks, Inc.
-
-project = matlab.project.currentProject;
-runtests(project.RootFolder);
-
-end
